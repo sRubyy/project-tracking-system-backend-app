6d9459134e23b39102b948c054ee7b2e47871d58
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-09 01:18:54 +0700

    test: create AssignmentService and EmployeeService unit test

---
+++ src/test/java/com/pfa/app/controller/AdminLocationControllerTest.java
@@ -0,0 +1,93 @@
+package com.pfa.app.controller;
+
+import com.pfa.app.exception.custom.ObjectNotFoundException;
+import com.pfa.app.model.entity.Location;
+import com.pfa.app.service.LocationService;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminLocationControllerTest {
+
+    @Mock
+    private LocationService locationServiceMock;
+
+    @Autowired
+    private LocationService locationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * See README.md - AdminLocationController.displayAdminLocationsEdit(@RequestParam("locationId") final String locationId, final Model model)`
+     * */
+    @Nested
+    class TestDisplayAdminLocationsEdit{
+
+        // Test case 1: ("", Not found, Throw exception)
+        @Test
+        @WithMockUser(username = "admin", roles = "ADMIN")
+        public void testEmptyLocationIdCantResponse() throws Exception {
+            assertThrows(NumberFormatException.class, () ->{
+                locationService.findById(Integer.parseInt(""));
+            });
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/admins/locations/admin-locations-edit")
+                            .param("locationId", ""))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
+
+        // Test case 2: ("1", Found, get response of target locationId)
+        @Test
+        @WithMockUser(username = "admin", roles = "ADMIN")
+        public void testLocationIdCanResponse() throws Exception {
+            Location location = new Location();
+
+            when(locationServiceMock.findById(Integer.parseInt("1"))).thenReturn(location);
+
+            assertEquals(location, locationServiceMock.findById(1));
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/admins/locations/admin-locations-edit")
+                            .param("locationId", "1"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.view().name("admins/locations/admin-locations-edit"))
+                    .andExpect(MockMvcResultMatchers.model().attributeExists("location"))
+                    .andReturn();
+        }
+
+        // Test case 3: ("3", Not found, Throw exception)
+        @Test
+        @WithMockUser(username = "admin", roles = "ADMIN")
+        public void testLocationIdCantResponseNotFound() throws Exception {
+            when(locationServiceMock.findById(Integer.parseInt("3"))).thenThrow(ObjectNotFoundException.class);
+
+            assertThrows(ObjectNotFoundException.class, () ->{
+                locationServiceMock.findById(Integer.parseInt("3"));
+            });
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/admins/locations/admin-locations-edit")
+                            .param("locationId", "3"))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
+    }
+
+}
