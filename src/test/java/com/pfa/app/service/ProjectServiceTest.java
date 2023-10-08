88ff7edaa5bee42f5f803261a8ed3f46f1fde9a7
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-09 01:18:54 +0700

    test: create ProjectService unit test

---
+++ src/test/java/com/pfa/app/service/ProjectServiceTest.java
@@ -0,0 +1,50 @@
+package com.pfa.app.service;
+
+import com.pfa.app.model.dto.ProjectCommitInfoDTO;
+import com.pfa.app.model.entity.Project;
+import com.pfa.app.service.ProjectService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectServiceTest {
+
+    @Mock
+    private ProjectService projectService;
+
+    /**
+     * See README.md - ProjectService.findById(final Integer projectId)
+     * */
+    @Test
+    public void testFindById() {
+        // Test Case 1: (1, True, False)
+        Project expectedProject = new Project();
+        int projectId = 1;
+        when(projectService.findById(eq(projectId))).thenReturn(expectedProject);
+
+        Project resultProject = projectService.findById(projectId);
+        assertThat(resultProject).isEqualTo(expectedProject);
+    }
+
+    /**
+     * See README.md - ProjectService.findByUsernameAndProjectId(final String username, final Integer projectId)
+     * */
+    @Test
+    public void testFindByUsernameAndProjectId() {
+        // Test Case 1: (False, 1, True, False)
+        ProjectCommitInfoDTO expectedProjectCommitInfoDTO = mock(ProjectCommitInfoDTO.class);
+        String username = "Anders Hejlsberg";
+        int projectId = 1;
+        when(projectService.findByUsernameAndProjectId(eq(username), eq(projectId))).thenReturn(expectedProjectCommitInfoDTO);
+
+        ProjectCommitInfoDTO resultProjectCommitInfoDTO = projectService.findByUsernameAndProjectId(username, projectId);
+        assertThat(resultProjectCommitInfoDTO).isEqualTo(expectedProjectCommitInfoDTO);
+    }
+}
